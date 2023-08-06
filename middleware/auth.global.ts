bfd55b3392d19b1994105e7a1019f67c2226edea
Aleks <aleksandar.trpkovski@gmail.com>
2023-08-06 22:01:22 +1000

    refactor(auth.global.ts): simplify authentication middleware logic for redirecting users
    The logic for redirecting users based on their authentication status has been simplified in the `auth.global.ts` middleware file. The previous implementation used conditional statements and the `useAuthStore` function from the `@/stores/auth` module.
    
    The updated implementation now uses the `$auth` object from the `useNuxtApp` function to check the authentication status. The code has been refactored to remove unnecessary conditional statements and improve readability.
    
    If the user is authenticated and tries to access the root path ("/"), they will be redirected to the dashboard ("/dashboard"). If the user is not authenticated and tries to access any path under "/dashboard", they will be redirected to the root path ("/").

--- middleware/auth.global.ts
+++ middleware/auth.global.ts
@@ -1,15 +1,10 @@
-import { useAuthStore } from "@/stores/auth";
-
 export default defineNuxtRouteMiddleware(async (to, from) => {
-    if (process.client) {
-        const { $auth } = useNuxtApp();
+    const { $auth } = useNuxtApp();
+    if (to.path === "/" && (await $auth?.isAuthenticated())) {
+        return navigateTo("/dashboard");
+    }
 
-        if (to.path === "/" && (await $auth.isAuthenticated())) {
-            return navigateTo("/dashboard");
-        }
-
-        if (to.path.includes("/dashboard") && !(await $auth.isAuthenticated())) {
-            return navigateTo("/");
-        }
+    if (to.path.includes("/dashboard") && !(await $auth?.isAuthenticated())) {
+        return navigateTo("/");
     }
 });
