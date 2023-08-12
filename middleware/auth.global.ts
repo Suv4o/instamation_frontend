624f4f0d927c997bc19833a9120514ad1c6f549b
Aleks <aleksandar.trpkovski@gmail.com>
2023-08-12 15:08:07 +1000

    fix(auth.global.ts): fix logic for redirecting to login page when not authenticated feat(auth.global.ts): add logic to redirect to dashboard page when authenticated and trying to access login page feat(auth.global.ts): add logic to redirect to login page when trying to access dashboard page without authentication

--- middleware/auth.global.ts
+++ middleware/auth.global.ts
@@ -1,10 +1,14 @@
 export default defineNuxtRouteMiddleware(async (to, from) => {
     const { $auth } = useNuxtApp();
-    if (to.path === "/" && (await $auth?.isAuthenticated())) {
+    if (to.path === "/" && from.path !== "/login") {
+        return navigateTo("/login");
+    }
+
+    if (to.path === "/login" && (await $auth?.isAuthenticated())) {
         return navigateTo("/dashboard");
     }
 
     if (to.path.includes("/dashboard") && !(await $auth?.isAuthenticated())) {
-        return navigateTo("/");
+        return navigateTo("/login");
     }
 });
